81d81e1ab22dd7e7b73795955a890d10d19deb44
juner <juner@juner.net>
2019-02-13 08:41:55 +0900

    BinaryReader を継承する様に修正

--- EndianStream/EndianBinaryReader.cs
+++ EndianStream/EndianBinaryReader.cs
@@ -1,193 +1,19 @@
 ﻿using System;
 using System.IO;
+using System.Security;
 using System.Text;
 
 namespace EndianStream
 {
-    public class EndianBinaryReader : IDisposable
+    public class EndianBinaryReader : BinaryReader, IDisposable
     {
-        const int MaxCharBytesSize = 128;
         bool IsLittleEndian { get; }
-        Decoder Decoder;
-        Stream Input;
-        byte[] Buffer;
-        byte[] CharBytes;
-        char[] SingleChar;
-        char[] CharBuffer;
-        int MaxCharSize;
-        bool Is2BytePerChar;
-        bool IsMemoryStream;
-        bool LeaveOpen;
         public EndianBinaryReader(Stream Input) : this(Input, new UTF8Encoding(), System.BitConverter.IsLittleEndian, false) { }
         public EndianBinaryReader(Stream Input, bool IsLittleEndian) : this(Input, new UTF8Encoding(), IsLittleEndian, false) { }
         public EndianBinaryReader(Stream Input, Encoding Encoding) : this(Input, Encoding, System.BitConverter.IsLittleEndian, false) { }
         public EndianBinaryReader(Stream Input, Encoding Encoding, bool IsLittleEndian) : this(Input, Encoding, IsLittleEndian, false) { }
-        public EndianBinaryReader(Stream Input, Encoding Encoding, bool IsLittleEndian, bool LeaveOpen)
-        {
-            this.Input = Input ?? throw new ArgumentNullException(nameof(Input));
-            Decoder = (Encoding ?? throw new ArgumentNullException(nameof(Encoding))).GetDecoder();
-            MaxCharSize = Encoding.GetMaxCharCount(MaxCharBytesSize);
-            if (!Input.CanRead)
-                throw new ArgumentException($"{nameof(Input)} is {nameof(Input.CanRead)} != {true}");
-            this.IsLittleEndian = IsLittleEndian;
-            var MinBufferSize = Encoding.GetMaxByteCount(1);  // max bytes per one char
-            if (MinBufferSize < 16)
-                MinBufferSize = 16;
-            Buffer = new byte[MinBufferSize];
-            // m_charBuffer and m_charBytes will be left null.
-
-            // For Encodings that always use 2 bytes per char (or more), 
-            // special case them here to make Read() & Peek() faster.
-            Is2BytePerChar = Encoding is UnicodeEncoding;
-            IsMemoryStream = Input.GetType() == typeof(MemoryStream);
-            this.LeaveOpen = LeaveOpen;
-        }
-        //
-        // 概要:
-        //     Returns the next available character and does not advance the byte or character
-        //     position.
-        //
-        // 戻り値:
-        //     The next available character, or -1 if no more characters are available or the
-        //     stream does not support seeking.
-        //
-        // 例外:
-        //   T:System.IO.IOException:
-        //     An I/O error occurs.
-        //
-        //   T:System.ArgumentException:
-        //     The current character cannot be decoded into the internal character buffer by
-        //     using the System.Text.Encoding selected for the stream.
-        public virtual int PeekChar();
-        //
-        // 概要:
-        //     Reads characters from the underlying stream and advances the current position
-        //     of the stream in accordance with the Encoding used and the specific character
-        //     being read from the stream.
-        //
-        // 戻り値:
-        //     The next character from the input stream, or -1 if no characters are currently
-        //     available.
-        //
-        // 例外:
-        //   T:System.IO.IOException:
-        //     An I/O error occurs.
-        //
-        //   T:System.ObjectDisposedException:
-        //     The stream is closed.
-        public virtual int Read();
-        //
-        // 概要:
-        //     Reads the specified number of bytes from the stream, starting from a specified
-        //     point in the byte array.
-        //
-        // パラメーター:
-        //   buffer:
-        //     The buffer to read data into.
-        //
-        //   index:
-        //     The starting point in the buffer at which to begin reading into the buffer.
-        //
-        //   count:
-        //     The number of bytes to read.
-        //
-        // 戻り値:
-        //     The number of bytes read into buffer. This might be less than the number of bytes
-        //     requested if that many bytes are not available, or it might be zero if the end
-        //     of the stream is reached.
-        //
-        // 例外:
-        //   T:System.ArgumentException:
-        //     The buffer length minus index is less than count. -or- The number of decoded
-        //     characters to read is greater than count. This can happen if a Unicode decoder
-        //     returns fallback characters or a surrogate pair.
-        //
-        //   T:System.ArgumentNullException:
-        //     buffer is null.
-        //
-        //   T:System.ArgumentOutOfRangeException:
-        //     index or count is negative.
-        //
-        //   T:System.ObjectDisposedException:
-        //     The stream is closed.
-        //
-        //   T:System.IO.IOException:
-        //     An I/O error occurs.
-        public virtual int Read(byte[] buffer, int index, int count);
-        //
-        // 概要:
-        //     Reads the specified number of characters from the stream, starting from a specified
-        //     point in the character array.
-        //
-        // パラメーター:
-        //   buffer:
-        //     The buffer to read data into.
-        //
-        //   index:
-        //     The starting point in the buffer at which to begin reading into the buffer.
-        //
-        //   count:
-        //     The number of characters to read.
-        //
-        // 戻り値:
-        //     The total number of characters read into the buffer. This might be less than
-        //     the number of characters requested if that many characters are not currently
-        //     available, or it might be zero if the end of the stream is reached.
-        //
-        // 例外:
-        //   T:System.ArgumentException:
-        //     The buffer length minus index is less than count. -or- The number of decoded
-        //     characters to read is greater than count. This can happen if a Unicode decoder
-        //     returns fallback characters or a surrogate pair.
-        //
-        //   T:System.ArgumentNullException:
-        //     buffer is null.
-        //
-        //   T:System.ArgumentOutOfRangeException:
-        //     index or count is negative.
-        //
-        //   T:System.ObjectDisposedException:
-        //     The stream is closed.
-        //
-        //   T:System.IO.IOException:
-        //     An I/O error occurs.
-        public virtual int Read(char[] buffer, int index, int count);
-        //
-        // 概要:
-        //     Reads a Boolean value from the current stream and advances the current position
-        //     of the stream by one byte.
-        //
-        // 戻り値:
-        //     true if the byte is nonzero; otherwise, false.
-        //
-        // 例外:
-        //   T:System.IO.EndOfStreamException:
-        //     The end of the stream is reached.
-        //
-        //   T:System.ObjectDisposedException:
-        //     The stream is closed.
-        //
-        //   T:System.IO.IOException:
-        //     An I/O error occurs.
-        public virtual bool ReadBoolean();
-        //
-        // 概要:
-        //     Reads the next byte from the current stream and advances the current position
-        //     of the stream by one byte.
-        //
-        // 戻り値:
-        //     The next byte read from the current stream.
-        //
-        // 例外:
-        //   T:System.IO.EndOfStreamException:
-        //     The end of the stream is reached.
-        //
-        //   T:System.ObjectDisposedException:
-        //     The stream is closed.
-        //
-        //   T:System.IO.IOException:
-        //     An I/O error occurs.
-        public virtual byte ReadByte();
+        public EndianBinaryReader(Stream Input, Encoding Encoding, bool IsLittleEndian, bool LeaveOpen) : base(Input, Encoding, LeaveOpen)
+            => this.IsLittleEndian = IsLittleEndian;
         //
         // 概要:
         //     Reads the specified number of bytes from the current stream into a byte array
@@ -542,5 +368,6 @@ namespace EndianStream
             Dispose(true);
         }
         #endregion
-    }   
+    }
+}
 }
